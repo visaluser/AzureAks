c1b1dea2c9d23585e9e6e3bfbab073d4eedc2515
visaluser <132979509+visaluser@users.noreply.github.com>
2023-10-12 08:39:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "azurerm" {
      resource_group_name  = "visal-group"
-    #storage_account_name = "visalstorageok"
-    #container_name       = "visalok"
-    #key                  = "terraform.tfstate"
+     storage_account_name = "visalstorageok"
+     container_name       = "visalok"
+     key                  = "terraform01.tfstate"
   }
 }
-##############
