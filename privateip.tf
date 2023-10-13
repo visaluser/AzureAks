ce3dde1fc52753b29c9135d96f1b667da2681c86
visaluser <132979509+visaluser@users.noreply.github.com>
2023-10-13 11:14:03 +0530

    Update privateip.tf

--- privateip.tf
+++ privateip.tf
@@ -6,6 +6,7 @@ resource "azurerm_network_interface" "example" {
     name                          = "example-nic-ipconfig-dev"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = null
+   # public_ip_address_id          = null
+    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
